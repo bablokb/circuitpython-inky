d66c37f1a1badced6c851cfc6cac74effa344335
Bernhard Bablok <bablokb@gmx.de>
2025-05-10 14:50:23 +0200

    specify tests by name

--- examples/multicolor/main.py
+++ examples/multicolor/main.py
@@ -12,18 +12,21 @@
 # -------------------------------------------------------------------------
 
 # Tests:
-#   0 show_colors_v
-#   1 show_colors_h
-#   2 show_image
-#   3 blink_leds
-#   4 use_buttons
-#   5 black
-#   6 white
+TESTS = [
+  "show_colors_v",
+  "show_colors_h",
+  "show_image",
+  "blink_leds",
+  "use_buttons",
+  "black",
+  "white",
+  ]
+
+my_tests = ["black", "white"]
 
 # --- imports   ------------------------------------------------------------
 
 import time
-import supervisor
 import board
 import time
 import gc
@@ -31,21 +34,29 @@ import gc
 # --- select DUT and tests   ----------------------------------------------
 
 from inky_imp import InkyImpression57 as Inky
-my_tests = [6]
-inky = Inky()
 
 # --- main program   ----------------------------------------------------
 
-while not supervisor.runtime.serial_connected:
-  time.sleep(1)
+time.sleep(10)
 print(f"running on board {board.board_id}")
+inky = Inky()
 
-for tst in inky.tests(my_tests):
-  print(f"running test: {tst}")
-  tst()
-  print(f"finished: {tst}",int(time.monotonic()))
-  #gc.collect()
-  #time.sleep(10)
+for tst in my_tests:
+  start = time.monotonic()
+  print(f"[{start:0.1f}] starting: {tst}()")
+  getattr(inky,tst)()
+  end = time.monotonic()
+  print(f"[{end:0.1f}] finished: {tst}() (duration: {end-start:0.1f})")
+  gc.collect()
+
+  # wait the configured time-to-refresh
+  ttr = inky.display.time_to_refresh
+  if ttr:
+    start = time.monotonic()
+    print(f"[{start:0.1f}] waiting {ttr}s")
+    time.sleep(ttr)
+    end = time.monotonic()
+    print(f"[{end:0.1f}] finished waiting")
 #inky.deinit()
 
 while True:
